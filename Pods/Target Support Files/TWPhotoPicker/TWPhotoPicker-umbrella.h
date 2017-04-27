1cc6589e935f0b732d34a6d6163150bea4cb3e73
Dhruv Shah <dhruv.shah@gmail.com>
2017-04-26 20:46:04 -0700

    um

--- Pods/Target Support Files/TWPhotoPicker/TWPhotoPicker-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "TWPhotoPickerController.h"
-#import "TWPhotoCollectionViewCell.h"
-#import "TWImageScrollView.h"
-
-FOUNDATION_EXPORT double TWPhotoPickerVersionNumber;
-FOUNDATION_EXPORT const unsigned char TWPhotoPickerVersionString[];
-
